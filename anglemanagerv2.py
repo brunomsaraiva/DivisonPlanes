b5a26b650782512a9f7f3c7b8c24f3eebc852381
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2019-08-05 15:42:49 +0100

    added a new method based on eigenvectors to determine orientation of kymographs

--- anglemanagerv2.py
+++ anglemanagerv2.py
@@ -1,3 +1,4 @@
+import cv2
 import math
 import numpy as np
 import tkinter.filedialog as fd
@@ -6,9 +7,10 @@ from skimage.draw import line
 from skimage.io import imread, imsave
 from skimage.filters import threshold_isodata
 from skimage.color import gray2rgb
-from skimage.util import img_as_float
+from skimage.util import img_as_float, img_as_uint
 from skimage.exposure import rescale_intensity
 from skimage.feature import peak_local_max
+from sklearn.decomposition import PCA
 from scipy import stats, ndimage
 from scipy import odr
 from scipy.spatial import ConvexHull
@@ -19,8 +21,9 @@ from sklearn.linear_model import LinearRegression
 class AngleManager(object):
 
     def __init__(self):
-        self.pixel_size = 32.5
-        self.z_step = 150
+        # Elyra settings
+        self.pixel_size = 32.2
+        self.z_step = 110
 
         self.kymograph_1 = None
         self.kymograph_2 = None
@@ -162,48 +165,78 @@ class AngleManager(object):
             y0 = int((left_bot[1]+right_bot[1])/2)
             x1 = int((left_top[0]+right_top[0])/2)
             y1 = int((left_top[1]+right_top[1])/2)
-            print(x0, y0, x1, y1)
 
         return [[x0, y0], [x1, y1], orientation]
 
     def lin_func(self, params, x):
         return params[0] * x + params[1]
 
+    def linear_function(self, p, x):
+        m, c = p
+        return m*x + c
+
+    def pcamethod(self, filtered_kym):
+        # get x and y points of binary image
+        x, y = np.nonzero(filtered_kym)
+        x = [[val] for val in x]
+        y = [[val] for val in y]
+
+        coords = np.concatenate((x, y), axis=1)
+
+        pca = PCA(n_components=1)
+        pca.fit(coords)
+
+        pos_x, pos_y = pca.mean_
+        eigenvector_x, eigenvector_y = pca.components_[0]
+        eigenval = pca.explained_variance_[0]
+
+        return [[pos_x-eigenvector_x*eigenval, pos_y-eigenvector_y*eigenval], [pos_x+eigenvector_x*eigenval, pos_y+eigenvector_y*eigenval]]
+
     def linreg(self, filtered_kym):
-        nonzero_points = np.nonzero(filtered_kym)
-        x_points = np.array(nonzero_points[0])
-        y_points = np.array(nonzero_points[1])
 
-        x_mean = np.mean(x_points)
-        y_mean = np.mean(y_points)
+        linear_model = odr.Model(self.linear_function)
+        x_points, y_points = np.nonzero(filtered_kym)
+        mydata = odr.RealData(x_points, y_points)
+        myodr = odr.ODR(mydata, linear_model, beta0=[0., 1.])
+        myodr.run()
+        m, b = myodr.output.beta
+        print(m, b)
 
-        C = np.cov([x_points-x_mean, y_points-y_mean])
-        evals, evecs = np.linalg.eig(C)
+        if b < 0:
+            y0 = 0
+            x0 = int(-b/m)
+            y1 = 1
+            x1 = int((1-b)/m)
+        elif b == 0:
+            x0 = 0
+            y0 = 0
+            x1 = 1
+            y1 = m
+        else:
+            y0 = int(b) - 1
+            x0 = int((y0-b)/m)
+            y1 = int(b) - 2
+            x1 = int((y1-b)/m)
 
-        a = evecs[1, evals.argmax()]/evecs[0, evals.argmax()]
-        b = y_mean - a * x_mean
-
-        x0 = np.min(np.array(nonzero_points[0]))
-        y0 = a * x0 + b
-        x1 = np.max(np.array(nonzero_points[0]))
-        y1 = a * x1 + b
-
-        print(x0, y0, x1, y1)
 
         return [[x0, y0], [x1, y1]]
 
 
-    def compute_coords(self, method="LinReg"):
+    def compute_coords(self, method="Box"):
 
-        kym1_sept_threshold = threshold_isodata(self.kymograph_1)
-        kym2_sept_threshold = threshold_isodata(self.kymograph_2)
+        # kym1_sept_threshold = threshold_isodata(self.kymograph_1)
+        # kym2_sept_threshold = threshold_isodata(self.kymograph_2)
+
+        kym1_sept_threshold = np.percentile(self.kymograph_1.flatten(), 95)
+        kym2_sept_threshold = np.percentile(self.kymograph_2.flatten(), 95)
+
+        self.filtered_kym1 = ndimage.morphology.distance_transform_edt(self.filtered_kym1) > 1.2
+        self.filtered_kym2 = ndimage.morphology.distance_transform_edt(self.filtered_kym2) > 1.2
 
         self.filtered_kym1 = self.kymograph_1 > kym1_sept_threshold
         self.filtered_kym2 = self.kymograph_2 > kym2_sept_threshold
 
         if method == "Box":
-            self.filtered_kym1 = ndimage.morphology.distance_transform_edt(self.filtered_kym1) > 1.2
-            self.filtered_kym2 = ndimage.morphology.distance_transform_edt(self.filtered_kym2) > 1.2
 
             self.kym1_rectangle_coords = self.find_minimum_bounding_box(self.filtered_kym1)
             self.kym2_rectangle_coords = self.find_minimum_bounding_box(self.filtered_kym2)
@@ -215,13 +248,29 @@ class AngleManager(object):
             self.kym1_axis = self.linreg(self.filtered_kym1)
             self.kym2_axis = self.linreg(self.filtered_kym2)
 
+        elif method == "PCA":
+            self.kym1_axis = self.pcamethod(self.filtered_kym1)
+            self.kym2_axis = self.pcamethod(self.filtered_kym2)
+
     def calculate_line_points(self, kym, kym_axis):
 
         x0, y0 = kym_axis[0]
         x1, y1 = kym_axis[1]
-        x_points, y_points = line(x0, int(y0), x1, int(y1))
+        x_points, y_points = line(int(x0), int(y0), int(x1), int(y1))
 
-        return [x_points, y_points]
+        max_x, max_y = kym.shape
+
+
+        fixed_x_points = []
+        fixed_y_points = []
+
+        for i in range(len(x_points)):
+
+            if 0 < x_points[i] < max_x and 0 < y_points[i] < max_y:
+                fixed_x_points.append(x_points[i])
+                fixed_y_points.append(y_points[i])
+
+        return [fixed_x_points, fixed_y_points]
 
     def compute_regression(self):
 
@@ -272,7 +321,6 @@ class AngleManager(object):
 
             angle = math.degrees(math.atan(opposite/adjacent)) * direction
 
-
         return angle
 
     def compute_angles(self):
@@ -281,4 +329,6 @@ class AngleManager(object):
         self.kym2_angle = self.angle_from_points(self.kym2_axis)
 
         self.angle_diff = abs(self.kym1_angle - self.kym2_angle)
+        if self.angle_diff > 90.0:
+            self.angle_diff = 180 - self.angle_diff
 
--- batchanalysis.py
+++ batchanalysis.py
@@ -18,24 +18,30 @@ HTML_HEADER = """<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
 root_folder = fd.askdirectory()
 
 report = HTML_HEADER
-report += '<table cellpading=10 cellspacing=10>\n<th>Cell Name</th><th>Line1</th><th>Kym1</th><th>Kym1 Angle</th>'
-report += '<th>Line2</th><th>Kym2</th><th>Kym2 Angle</th><th>Angle Difference</th>\n'
+report += '<table cellpading=10 cellspacing=10>\n<th>Cell Name</th><th>Line1</th><th>Kym1</th><th>Kym1 Filtered</th><th>Kym1 Angle</th>'
+report += '<th>Line2</th><th>Kym2</th><th>Kym2 Filtered</th><th>Kym2 Angle</th><th>Angle Difference</th>\n'
 for cell in os.listdir(root_folder):
+
     working_path = root_folder + os.sep + cell
     app = AngleManager()
     app.load_kymographs(path_kym1=working_path+os.sep+"kym1.tif", path_kym2=working_path+os.sep+"kym2.tif")
-    app.compute_coords(method="Box")
+    print(cell)
+    app.compute_coords(method="PCA")
     app.compute_regression()
     app.compute_angles()
 
     imsave(working_path+os.sep+"kym1_w_line.png", img_as_ubyte(app.kymograph_1_w_line))
     imsave(working_path+os.sep+"kym2_w_line.png", img_as_ubyte(app.kymograph_2_w_line))
+    imsave(working_path+os.sep+"kym1_filtered.png", img_as_ubyte(app.filtered_kym1))
+    imsave(working_path+os.sep+"kym2_filtered.png", img_as_ubyte(app.filtered_kym2))
 
     report += '<tr><td valign="middle" width="100" align="center">' + cell + '</td><td valign="middle"  align="center"><img src="./' + cell + '/kymline1.png" alt="pic" width="200"/></td>'
-    report += '<td valign="middle" align="center"><img src="./' + cell + '/kym1_w_line.png" alt="pic"/></td>'
+    report += '<td valign="middle" align="center"><img src="./' + cell + '/kym1_w_line.png" alt="pic" width="200"/></td>'
+    report += '<td valign="middle" align="center"><img src="./' + cell + '/kym1_filtered.png" alt="pic" width="200"/></td>'
     report += '<td valign="middle" width="100" align="center">' + ("{0:.2f}").format(app.kym1_angle) + '</td>'
-    report += '<td valign="middle" align="center"><img src="./' + cell + '/kymline2.png" alt="pic"/></td>'
-    report += '<td valign="middle" align="center"><img src="./' + cell + '/kym2_w_line.png" alt="pic"/></td>'
+    report += '<td valign="middle" align="center"><img src="./' + cell + '/kymline2.png" alt="pic" width="200"/></td>'
+    report += '<td valign="middle" align="center"><img src="./' + cell + '/kym2_w_line.png" alt="pic" width="200"/></td>'
+    report += '<td valign="middle" align="center"><img src="./' + cell + '/kym2_filtered.png" alt="pic" width="200"/></td>'
     report += '<td valign="middle" width="100" align="center">' + ("{0:.2f}").format(app.kym2_angle) + '</td>'
     report += '<td valign="middle" width="100" align="center">' + ("{0:.2f}").format(app.angle_diff) + '</td></tr>\n'
 
